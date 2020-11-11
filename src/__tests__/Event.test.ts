eadc7019c118a2d72c0bf91a4bd89962a83d87e8
Alexander Sage <sagezander@gmail.com>
2020-11-11 00:16:40 -0500

    chore: added type checking

--- src/__tests__/Event.test.ts
+++ src/__tests__/Event.test.ts
@@ -4,7 +4,7 @@ describe(Event.name, () => {
     const cb1 = jest.fn()
     const cb2 = jest.fn()
     const cb3 = jest.fn()
-    const event = Event()
+    const event = Event<any>()
     event.on(cb1)
     event.on(cb2)
     event.on(cb3)
@@ -19,7 +19,7 @@ describe(Event.name, () => {
   it('should remove listeners if off is called or the return function from on', () => {
     const cb1 = jest.fn()
     const cb2 = jest.fn()
-    const event = Event()
+    const event = Event<any>()
     event.on(cb1)
     const killEvent = event.on(cb2)
     event.emit('Call1')
@@ -48,7 +48,7 @@ describe(Event.name, () => {
     const cb1 = jest.fn()
     const cb2 = jest.fn()
     const cb3 = jest.fn()
-    const event = Event()
+    const event = Event<any>()
     event.on(cb1)
     event.on(cb2)
     event.on(cb3)
@@ -64,8 +64,9 @@ describe(Event.name, () => {
   })
   it('should emit any kind of event', () => {
     const cb = jest.fn()
-    const event = Event()
+    const event = Event<any>()
     event.on(cb)
+    // @ts-expect-error
     event.emit()
     expect(cb).toHaveBeenCalledWith()
     event.emit('')
@@ -77,34 +78,40 @@ describe(Event.name, () => {
   })
   it('should only emit the first argument', () => {
     const cb = jest.fn()
-    const event = Event()
+    const event = Event<any>()
     event.on(cb)
+    // @ts-expect-error
     event.emit('a', 'b', 'c')
     expect(cb).toHaveBeenCalledTimes(1)
     expect(cb).toHaveBeenCalledWith('a')
   })
   it('SAFEGUARD: when emit is called without any callbacks', () => {
-    const event = Event()
+    const event = Event<any>()
     event.emit({})
   })
 
   it('SAFEGUARD: when on is called without a callback', () => {
-    const event = Event()
+    const event = Event<any>()
+    // @ts-expect-error
     expect(() => event.on('invalid yo')).not.toThrow()
+    // @ts-expect-error
     expect(() => event.on()).not.toThrow()
   })
   it('SAFEGUARD: when on is called without a callback and the return value is called', () => {
-    const event = Event()
+    const event = Event<any>()
+    // @ts-expect-error
     const killEvent = event.on('invalid yo')
     expect(() => killEvent()).not.toThrow()
   })
   it('SAFEGUARD: when off is called with an unknown function', () => {
-    const event = Event()
+    const event = Event<any>()
     expect(() => event.off(() => {})).not.toThrow()
   })
   it('SAFEGUARD: when off is called without a callback', () => {
-    const event = Event()
+    const event = Event<any>()
+    // @ts-expect-error
     expect(() => event.off('invalid callback')).not.toThrow()
+    // @ts-expect-error
     expect(() => event.off()).not.toThrow()
   })
 })
