1904f285563263b813cb91574b156d81640549d8
Alexander Sage <sagezander@gmail.com>
2020-11-13 20:41:42 -0500

    feat: Added local object storage (#8)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { Event } from './Event'
+export { LocalObjectStorage } from './LocalObjectStorage'
---
+++ src/utils/logger.ts
@@ -0,0 +1,6 @@
+export const error: typeof console.error = (...args: any[]) => {
+  return console.error(...args)
+}
+export const warn: typeof console.warn = (...args: any[]) => {
+  return console.warn(...args)
+}
