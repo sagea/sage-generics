cba11abe8dae71fa4cb7e85a4d22e807d92e43b1
Alexander Sage <sagezander@gmail.com>
2020-11-14 20:47:11 -0500

    feat: Added simple state manager (#9)

---
+++ src/StateManagers/__tests__/SetStateManager.test.ts
@@ -0,0 +1,34 @@
+import { SetStateManager } from '../SetStateManager'
+
+describe(SetStateManager.name, () => {
+  it('should return default state initially', () => {
+    const store = SetStateManager({ foo: 'bar' })
+    expect(store.getState()).toMatchInlineSnapshot(`
+      Object {
+        "foo": "bar",
+      }
+    `)
+  })
+  it('should update state', () => {
+    const store = SetStateManager({ foo: 'bar' })
+    store.setState(prevState => {
+      return {
+        ...prevState,
+        foo: 'yoyoyo',
+      }
+    })
+    expect(store.getState()).toMatchInlineSnapshot(`
+      Object {
+        "foo": "yoyoyo",
+      }
+    `)
+  })
+  it('should emit a change event on state change', () => {
+    const callback = jest.fn()
+    const store = SetStateManager({ foo: 'bar' })
+    store.onChange(callback)
+    expect(callback).not.toHaveBeenCalled()
+    store.setState(() => ({ foo: 'woah' }))
+    expect(callback).toHaveBeenCalledTimes(1)
+  })
+})
