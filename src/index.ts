3f673f73189054dc6729ad25c5b3b8582bddf8e4
Alexander Sage <sagezander@gmail.com>
2020-12-02 18:37:57 -0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export { Event } from './Event.ts'
 export { LocalObjectStorage } from './LocalObjectStorage.ts'
 export { SetStateManager } from './StateManagers/SetStateManager.ts';
-
-console.log('index.ts 12');
